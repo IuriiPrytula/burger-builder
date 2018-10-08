44dc4b21fd62e5f375469ccea469278776eef6a8
Iurii Prytula <iurii.prytula@gmail.com>
2018-10-08 20:32:54 +0200

    add PropTypes

--- src/components/Burger/BurgerIngridient/BurgerIngridient.js
+++ src/components/Burger/BurgerIngridient/BurgerIngridient.js
@@ -1,39 +1,45 @@
-import React from "react"
+import React, { Component } from "react"
 import classes from "./BurgerIngriients.css"
+import PropTypes from "prop-types"
+class BurgerIngridient extends Component {
+  render() {
+    let ingridient = null
 
-const burgerIngridient = props => {
-  let ingridient = null
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingridient = <div className={classes.BreadBottom}>cla</div>
+        break
+      case "bread-top":
+        ingridient = (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1} />
+            <div className={classes.Seeds2} />
+          </div>
+        )
+        break
+      case "meat":
+        ingridient = <div className={classes.Meat} />
+        break
+      case "cheese":
+        ingridient = <div className={classes.Cheese} />
+        break
+      case "bacon":
+        ingridient = <div className={classes.Bacon} />
+        break
+      case "salad":
+        ingridient = <div className={classes.Salad} />
+        break
+      default:
+        ingridient = null
+        break
+    }
 
-  switch (props.type) {
-    case "bread-bottom":
-      ingridient = <div className={classes.BreadBottom}>cla</div>
-      break
-    case "bread-top":
-      ingridient = (
-        <div className={classes.BreadTop}>
-          <div className={classes.Seeds1} />
-          <div className={classes.Seeds2} />
-        </div>
-      )
-      break
-    case "meat":
-      ingridient = <div className={classes.Meat} />
-      break
-    case "cheese":
-      ingridient = <div className={classes.Cheese} />
-      break
-    case "bacon":
-      ingridient = <div className={classes.Bacon} />
-      break
-    case "salad":
-      ingridient = <div className={classes.Salad} />
-      break
-    default:
-      ingridient = null
-      break
+    return ingridient
   }
-
-  return ingridient
 }
 
-export default burgerIngridient
+BurgerIngridient.PropTypes = {
+  type: PropTypes.string.isRequired
+}
+
+export default BurgerIngridient
